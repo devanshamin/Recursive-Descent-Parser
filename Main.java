f8cf1422c33daec38fc1ebc4a6ba7e745a85bbd3
Devansh Amin <aminpatel97@gmail.com>
2020-02-20 19:51:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Parser p = new Parser();
-	    p.parse();
-	    System.out.println("The syntax of the source program is correct.");
+	    	p.parse();
+	    	System.out.println("The syntax of the source program is correct.");
 	}
 }
